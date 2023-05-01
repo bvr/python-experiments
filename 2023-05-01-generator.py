99dd40a36cfa7c1807fca589345e6754ba1db02a
Roman Hubacek <roman.hubacek@centrum.cz>
2023-05-01 20:55:14 +0200

    Image to ASCII and generators

---
+++ 2023-05-01-generator.py
@@ -0,0 +1,21 @@
+
+# from https://code-maven.com/slides/python/list-comprehension-generator
+
+def sqr(n):
+    print(f"sqr {n}")
+    return n ** 2
+
+numbers = [1, 3, 7]
+
+# list comprehension
+n1 = [ sqr(n) for n in numbers ]
+print("we have the list")
+for i in n1:
+    print(i)
+print("-------")
+
+# generator expression
+n2 = ( sqr(n) for n in numbers )
+print("we have the generator")
+for i in n2:
+    print(i)
