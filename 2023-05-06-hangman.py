e12dbdc10da73a676d3356f54ee2b30fbeb3995d
Roman Hubacek <roman.hubacek@centrum.cz>
2023-05-16 18:19:45 +0200

    guessing of words

--- 2023-05-06-hangman.py
+++ 2023-05-06-hangman.py
@@ -9,9 +9,18 @@ def get_words(input_file):
 def upper_ascii(s):
     return unicodedata.normalize('NFKD', s.upper()).encode('ASCII', 'ignore')
 
+def show_entry(guess, num):
+    print(str(num) + ': ' + ' '.join([c if visible else '-'  for c,visible in guess]))
+
+
 def play(word):
     to_guess = [(c,False) for c in word.upper()]
-    print(to_guess)
+    for guess_num in range(1, 7):
+        print(to_guess)
+        show_entry(to_guess, guess_num)
+        guess = upper_ascii(input("Guess letter: "))
+        to_guess = [(c, visible or guess == upper_ascii(c)) for c,visible in to_guess]
+    
 
 def main():
     words = get_words('hangman-words.txt')
