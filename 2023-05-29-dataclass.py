33a57b84ebbc8621f3f467ae7f97cf5abd7637da
Roman Hubacek <roman.hubacek@centrum.cz>
2023-05-29 17:30:17 +0200

    dataclass

---
+++ 2023-05-29-dataclass.py
@@ -0,0 +1,16 @@
+
+from dataclasses import dataclass
+
+@dataclass
+class InventoryItem:
+    """Class for keeping track of an item in inventory."""
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+ii = InventoryItem(name='Thing', unit_price=99, quantity_on_hand=20)
+
+print(ii.total_cost())
